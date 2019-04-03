76e079d4e9fa482f36e845f0b9c175656b37ef2f
herizaladk <herizaladk@gmail.com>
2019-04-03 15:23:56 +0700

    Add loading animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
+import 'splashscreen.dart';
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-void main() => runApp(MyApp());
 
-@override
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Container(
-              child: Image.asset('assets/logo.png',width:2.0,height:2.0,),
-              decoration: BoxDecoration(
-                  color: new Color(0xFF43B0F1),
-                  gradient: LinearGradient(
-                      colors: [new Color(0xFF43B0F1), new Color(0xffE8EEF1)])),
-            ),
-          ],
-        ),
+    return new SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: new AfterSplash(),
+      image: Image.asset('assets/logo.png'),
+      gradientBackground: new LinearGradient(colors: [Colors.blue, Colors.white], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      backgroundColor: Colors.white,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 100.0,
+      onClick: ()=>print("Malala"),
+      loaderColor: Colors.white,
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Text("Welcome!",
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30.0
+        ),),
+
       ),
     );
   }
