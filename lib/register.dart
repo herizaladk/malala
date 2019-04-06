4ea780418d32f74bf90e67268684d40800662e72
herizaladk <herizaladk@gmail.com>
2019-04-06 20:03:53 +0700

    Fixing all material

--- lib/register.dart
+++ lib/register.dart
@@ -23,32 +23,76 @@ class _RegisterState extends State<Register> {
                 'Sign Up',
                 style: TextStyle(fontSize: 36, color: Colors.white),
               ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: ('Fullname'),
+              // Material(
+              //   shape: RoundedRectangleBorder(
+              //       borderRadius: BorderRadius.circular(30.0)),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(40.0,10.0,40.0,10.0),
+                      child: TextFormField(
+                        style:new TextStyle(color:Colors.blue),
+                        autofocus: false,
+                        decoration: InputDecoration(
+                          fillColor:Colors.white,
+                          filled:true,
+                          hintText: 'Fullname',
+                          hintStyle: TextStyle(color:Colors.blue),
+                          contentPadding:
+                              EdgeInsets.fromLTRB(10.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                            // borderSide:const BorderSide(color:Colors.white),
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0,10.0,20.0,10.0),
+                      child: TextFormField(
+                        autofocus: false,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0,10.0,20.0,10.0),
+                      child: TextFormField(
+                        autofocus: false,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: ('Email'),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text(
+                      'By Clicking Register You Must Agree Policy Agreement',
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                    new Checkbox(
+                      value: (true),
+                      onChanged: (null),
+                    )
+                  ],
                 ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: ('Password'),
-                ),
-              ),
-              new Row(
-                children: <Widget>[
-                  new Text(
-                    'By Clicking Register You Must Agree Policy Agreement',
-                    style: TextStyle(fontSize: 12, color: Colors.white),
-                  ),
-                  new Checkbox(
-                    value:(true),
-                    onChanged:(null),
-                  )
-                ],
               ),
               new FlatButton(
                   shape: new RoundedRectangleBorder(
