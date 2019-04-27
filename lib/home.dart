4ccbee1d20fa95d6e6078c9b9fabf1b9ee221b93
herizaladk <herizaladk@gmail.com>
2019-04-27 11:37:59 +0700

    Weather API Connected

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'weather.dart' as weather;
+import 'dart:async';
+
 
 class Home extends StatefulWidget {
   const Home({Key key, this.user}) : super(key: key);
@@ -10,77 +15,96 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final PageController ctrl = PageController();
+
+  void showStuff() async {
+    Map data = await getweather(weather.appId, weather.defaultCity);
+    print(data.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-      return Scaffold(
-        backgroundColor: Colors.blueAccent,
-        body: Container(
-          margin: EdgeInsets.only(top: 50.0, right: 20.0, left: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Hello Jhon!',
-                    style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0, right: 20.0, left: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Hello Jhon!',
+                  style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
                   ),
-                  Container(
-                    width: 50.0,
-                    height: 50.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                  )
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Jatinangor',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+            SizedBox(
+              height: 10.0,
+            ),
+            new Container(
+                margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                child: updateTemp('Cileunyi')
+                ),
+            // Text(
+            //   '23C',
+            //   style: TextStyle(color: Colors.white, fontSize: 30.0),
+            // ),
+            SizedBox(height: 50.0),
+            Container(
+              height: 400,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  homeWidget(),
+                  homeWidget(),
+                  homeWidget(),
                 ],
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                'Bandung',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                '23C',
-                style: TextStyle(color: Colors.white, fontSize: 30.0),
-              ),
-              SizedBox(height:60.0),
-              Container(
-                height:400,
-                child:ListView(
-                  scrollDirection:Axis.horizontal,
-                  children: <Widget>[
-                    homeWidget(),
-                    homeWidget(),
-                    homeWidget(),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.only(bottom: 40.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.home), onPressed: () => showStuff()),
+            IconButton(icon: Icon(Icons.date_range), onPressed: () {}),
+          ],
+        ),
+      ),
+    );
     // );
   }
 
   Widget homeWidget() {
     return Container(
-      padding:EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(10.0),
       width: 250.0,
       height: 400.0,
       child: Stack(
@@ -115,11 +139,12 @@ class _HomeState extends State<Home> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                  'Dago Pakar',
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold, fontSize:18.0,)
-                ),
+                Text('Dago Pakar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    )),
                 Row(
                   children: <Widget>[
                     Icon(Icons.location_on, color: Colors.white),
@@ -135,5 +160,41 @@ class _HomeState extends State<Home> {
         ],
       ),
     );
+  }
+
+  Future<Map> getweather(String appId, String city) async {
+    String apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=$city,id&appid=${weather.appId}&units=metric';
+
+    http.Response response = await http.get(apiUrl);
+
+    return json.decode(response.body);
+  }
+
+  Widget updateTemp(String defaultcity) {
+    return new FutureBuilder(
+        future: getweather(weather.appId, weather.defaultCity),
+        builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+          if (snapshot.hasData) {
+            Map content = snapshot.data;
+            return new Container(
+              child: new Column(
+                children: <Widget>[
+                  new ListTile(
+                    title: new Text(content['main']['temp'].toString(),
+                    style: new TextStyle(
+                      fontStyle: FontStyle.normal,
+                      fontSize:20.0,
+                      color:Colors.white,
+                      fontWeight:FontWeight.w500
+                    ),),
+                  )
+                ],
+              ),
+            );
+          } else {
+            return new Container();
+          }
+        });
   }
 }
---
+++ lib/weather.dart
@@ -0,0 +1,2 @@
+final appId = '6ef4046d588a2e2079c68b513217c4cb' ;
+final defaultCity = 'Cileunyi';
